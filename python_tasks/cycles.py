0b3b85b802e12e65b2f8cb83e5cad222962fc5d7
moleculadesigner <moleculadesigner@gmail.com>
2017-10-24 17:54:22 +0700

    Started to solve simple part of course. ---MD

---
+++ python_tasks/cycles.py
@@ -0,0 +1,98 @@
+"""
+python_tasks/cycles.py
+
+Solution of basic tasks concerning cycles.
+
+Author:
+Danila Iakovlev
+"""
+import math as m
+
+def main():
+    print("1. Sequence from 1 to 10")
+    int_seq(10)
+    print("______\n")
+
+    print("2. Sequence of natural squares from 1 to 10")
+    sqr_seq(10)
+    print("______\n")
+
+    print("3. Backward sequence from 133 to 112")
+    backward_seq(103,112)
+    print("______\n")
+
+
+def int_seq(lim):
+    """
+    Returns natural numbers up to lim.
+    """
+
+    out_seq_str = "For:\t"
+    for i in range(lim):
+        out_seq_str += (str(i + 1))
+        if (i + 1) != lim:
+            out_seq_str += ", "
+       
+    print(out_seq_str)
+
+    out_seq_str = "While:\t"
+    i = 0
+    while i < lim:
+        i += 1
+        out_seq_str += str(i)
+        if i != lim:
+            out_seq_str += ", "
+
+    print(out_seq_str)
+  
+
+def sqr_seq(lim):
+    """
+    Prints sequence of natural squares up to lim.
+    """
+
+    out_seq_str = "For:\t"
+    for i in range(1,lim + 1,1):
+        out_seq_str += str(pow(i,2))
+        if i < lim:
+            out_seq_str += ", "
+    print(out_seq_str)
+
+    out_seq_str = "While:\t"
+    i = 0
+    while i < lim:
+        i += 1
+        out_seq_str += str(i*i)
+        if i < lim:
+            out_seq_str += ", "
+    print(out_seq_str)
+
+
+def backward_seq(max_num,min_num):
+    """
+    Prints backward cequence from max_num to min_num.
+    """
+    if max_num <= min_num:
+        print("Exception: invalid range.")
+        return "Exception: invalid range."
+
+    out_seq_str = "For:\t"
+    for i in range(max_num,min_num - 1,-1):
+        out_seq_str += str(i)
+        if i > min_num:
+            out_seq_str += ", "
+    print(out_seq_str)
+
+    out_seq_str = "While:\t"
+    j = max_num
+    while j >= min_num:
+        out_seq_str += str(j)
+        if j > min_num:
+            out_seq_str += ", "
+        j -= 1
+    print(out_seq_str)
+
+
+
+if __name__ == '__main__':
+    main()
