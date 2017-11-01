852faa2b1c37b197cbac1917ed40ebf709f49c3c
Danila Iakovlev <moleculadesigner@gmail.com>
2017-11-01 16:23:22 +0700

    Up to #4 is done ---MD

--- python_tasks/conditions_and_cycles.py
+++ python_tasks/conditions_and_cycles.py
@@ -15,6 +15,9 @@
 import math
 import random
 
+_even = True
+_odd = False
+
 def main():
 
     """
@@ -24,13 +27,28 @@ def main():
     print("Number " + str(user_int) + " is" + ("" if is_prime(user_int) else " not") + " prime.")
     print("______\n")
     print(str(int(True)) + " " + str(int(False)))
-    """
+
     # 2. Primes in interval
     print("2. Enter begin of prime range:")
     u_begin = get_int()
     print("   Enter end of prime range:")
-    u_end  = get_int()
+    u_end   = get_int()
     print("Primes here are:\n" + list_to_str(prime_interval(u_begin, u_end)))
+    print("______\n")
+
+    # 3. Even and odd numbered primes
+    position = _even
+    print("3. Primes with even position: \n" + list_to_str(prime_interval(),int(position),2))
+    position = _odd
+    print("\n   Primes with odd position: \n"  + list_to_str(prime_interval(),int(position),2))
+    print("______\n")
+    """
+
+    # 4. Quit command
+    print("4. Enter anything (q to skip):")
+    get_break('q')
+
+    # 5. 
 
 
 def get_int():
@@ -42,6 +60,17 @@ def get_int():
         print("This is not an integer, try again:")
         user_data = input()
     return int(user_data)
+
+
+def get_break(c):
+    """
+    Iteratively gets integer from std_in
+    """
+    user_data = input()
+    while not user_data == c:
+        print("This is (almost) endless loop, press " + c + " to break it:")
+        user_data = input()
+    return 0
 
 
 def is_integer(s):
@@ -61,7 +90,7 @@ def is_prime(number = 2):
     return True
 
 
-def prime_interval(begin = 2, end = 100000):
+def prime_interval(begin = 2, end = 1000):
     prime_list = []
     if (begin > end) or (begin < 2 and end < 2):
         return prime_list
@@ -73,16 +102,35 @@ def prime_interval(begin = 2, end = 100000):
     return prime_list
 
 
-def list_to_str(ls, row_len = 20):
+def list_to_str(ls, offset = 0, step = 1, row_len = 20):
+    """
+    ls - input list
+    offset - from what index to print
+    step - each index to be printed
+    row_len - how often will be line break
+    """
     out_s = ""
-    for i in range(len(ls)):
-        out_s += str(ls[i])
-        if i < len(ls) - 1:
-            out_s += "\t"
-        if i % row_len == row_len - 1:
-            out_s += "\n"
+    for i in range(offset, len(ls)):
+        if (i - offset) % step == 0:
+            out_s += str(ls[i])
+            if i < len(ls) - 1:
+                out_s += "\t" # + " :" + str(i) + ": "
+            if ((i - offset) / step) % row_len == row_len - 1:
+                out_s += "\n"
     return out_s
-    
+
+
+def bine_fibonacci(n):
+"""
+Implements Bine's formula for fibonacci numbers:
+F_n = round( phi^n - (-phi)^-n / (2*phi - 1) )
+"""
+
+    return 0
+
+def fibonacci_sum(cap, n_max, n_mode):
+    return 0
+
 
 if __name__ == '__main__':
     main()
