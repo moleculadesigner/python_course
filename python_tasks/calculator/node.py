d989bc79748127e8b26026fae98c69331f47a419
Danila Iakovlev <moleculadesigner@gmail.com>
2017-11-27 12:39:19 +0700

    Class Node() added ---MD

---
+++ python_tasks/calculator/node.py
@@ -0,0 +1,142 @@
+from functools import partial
+from copy import deepcopy, copy
+from inspect import signature
+
+#def partial(func, arg):
+#    def curry(*args):
+#        return func(arg, *args)
+#    return curry
+
+class Node():
+    """
+    A node/subtree in abstract synax tree.
+
+    Fields:
+    * `name` An operator name or 'leaf' if there are no child nodes.
+    * `expression` the leaf value or lambda expression if not a leaf.
+    * `children` a list of child nodes.
+
+    Methods:
+    * `grow(node)` Add a `node` into the left unsetttled child node.
+    * `render()` Returns a value of tree (process all subtrees).
+    * `strip()` Returns the rightest settled child node and clear it.
+    * `check()` Matches number of operands and number of children and returns **True** if correct.
+    * `show()` Returns string containing a schematic view of the node subtree.
+    """
+    name = str()
+    expression = 0
+    children = []
+    parent = None
+
+    def __init__(self, name = 'leaf', expression = 0, child_num = 0, parent = None):
+        """
+        Creates a new leaf or node with unsettled children.
+        """
+        self.name = name
+        self.expression = expression
+        self.children = []
+        for i in range(child_num):
+            self.children.append(None)
+        self.parent = parent
+        
+    def render(self):
+        """Returns a value of tree (process all subtrees)."""
+        if self.name == 'leaf':
+            return self.expression
+        result = self.expression
+        
+        for node in self.children:
+            if node != None:
+                result = partial(result, node.render())
+            else:
+                print("Render error: empty argument in function {}{}.".format(self.name, str(signature(result))))
+                raise ValueError
+            
+        #try:
+        #    result()
+        #except TypeError as ArgLack:
+        #    #print("Node.render(): syntax tree is incomplete, reterning a partially applied function instead of value.\nNode.render(): " + str(ArgLack))
+        #    print("return: func" + str(signature(result)))
+        #    return result
+        return result()
+
+    def grow(self, node):
+        if self.name == 'leaf' or not len(self.children):
+            return False
+        for i in range(len(self.children)):
+            if self.children[i] == None:
+                if not node.parent:
+                    self.children[i] = node
+                    node.parent = self
+                    return True
+            else:
+                self.children[i].grow(node)
+
+    def show(self, level = 0):
+        """Returns string containing a schematic view of the node subtree."""
+        s = ''
+        if self.name == 'leaf':
+            for i in range(level):
+                s += '┊\t'
+            return s + str(self.expression) + '\n'
+        for i in range(level):
+            s += '┊\t'
+        s += '╭ ' + self.name + '\n'
+        level += 1
+        for child in self.children:
+            if child:
+                s += child.show(level)
+            else:
+                for i in range(level):
+                    s += '┊\t'
+                s += '○\n' #∘○◌
+        return s
+
+        
+    
+
+"""
+"""
+def demo():
+    print("Abstract syntax tree class demo:")
+    print("1. Creating a '+' operator node n:\n>>> n = Node('+', lambda a, b: b + a, 2)")
+    n = Node('+', lambda a, b: b + a, 2)
+    print("\n2. Making a new node k with the same operator:\n>>> k = deepcopy(n)\n>>> l = deepcopy(n)")
+    k = deepcopy(n)
+    l = deepcopy(n)
+    print("\n3. Tuning it:\n>>> k.name = '•'\n>>> l.name = 'sum'\n>>> k.expression = lambda a, b: b * a")
+    k.name = '•'
+    l.name = 'sum'
+    k.expression = lambda a, b: b * a
+    print("\n4. Also we need some arguments (leaf nodes):\n>>> m = Node('leaf', 1) (m = 1)\n>>> o = Node('leaf', 5) (o = 5)\n>>> q = Node('leaf', 3) (q = 3)")
+    m = Node('leaf', 1)
+    o = Node('leaf', 5)
+    q = Node('leaf', 3)
+    print("\n5. Sometimes we need to draw a tree:\n>>> print(k.show())")
+    print(k.show())
+    print("\n6. Setting up a tree:\n>>> n.grow(m)")
+    n.grow(m)
+    print(n.show())
+    print(">>> n.grow(o)")
+    n.grow(o)
+    print(n.show())
+    print("A tree may be inserted into another tree:\n>>> k.grow(n)\n>>> k.grow(l)")
+    k.grow(n)
+    k.grow(l)
+    print(k.show())
+    
+    print("7. This tree represents a function of 2 arguments, so trying to render it will raise a ValueError:\n>>> k.render()")
+    try:
+        k.render()
+    except ValueError:
+        print('We cannot render functions yet, only complete expressions.\n')
+    print("8. Finalizing and render tree:\n>>> k.grow(q)")
+    print("And adding an absolutely new leaf:\n>>> k.grow(Node('leaf', 1.25))")
+    k.grow(q)
+    k.grow(Node('leaf', 1.25))
+    print(k.show())
+    print("3 1.25 + 1 5 + * = {}".format(k.render()))
+
+if __name__ == '__main__':
+    demo()
+
