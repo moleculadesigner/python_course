545914b1f191f4d99ab09122af17a91356c35a8d
Danila Iakovlev <moleculadesigner@gmail.com>
2017-11-28 17:02:35 +0700

    finished

--- python_tasks/calculator/node.py
+++ python_tasks/calculator/node.py
@@ -18,20 +18,21 @@ class Node():
     * `name` An operator name or 'leaf' if there are no child nodes.
     * `expression` the leaf value or lambda expression if not a leaf.
     * `children` a list of child nodes.
+    * `parent` link to a parent node.
 
     **Methods**:
     * `grow(node)` Add a `node` into the left unsetttled child node.
     * `render()` Returns a value of tree (process all subtrees).
-    * `strip()` Returns the rightest settled child node and clear it.
-    * `check()` Iteratively matches number of operands and number of children and returns **True** if correct.
+    * `freenodes()` Returns total number of empty nodes (**None**) in the tree. 
     * `show()` Returns string containing a schematic view of the node subtree.
     """
     name = str()
+    fname = ''
     expression = 0
     children = []
     parent = None
 
-    def __init__(self, name = leaf, expression = 0, child_num = 0, parent = None):
+    def __init__(self, name = leaf, expression = 0, child_num = 0, parent = None, fname = ''):
         """
         Creates a new leaf or node with unsettled children.
         """
@@ -41,6 +42,7 @@ class Node():
         for i in range(child_num):
             self.children.append(None)
         self.parent = parent
+        self.fname = fname
         
     def render(self):
         """Returns a value of tree (process all subtrees)."""
@@ -65,10 +67,10 @@ class Node():
 
     def grow(self, node):
         """Add a `node` into the left unsetttled child node."""
-        if self.name == leaf or self.name == link:
+        if self.name in [leaf, link]:
             return False
         for i in range(len(self.children)):
-            if self.children[i] == None:
+            if not self.children[i]:
                 if not node.parent:
                     self.children[i] = node
                     node.parent = self
@@ -82,14 +84,14 @@ class Node():
         if self.name == leaf:
             for i in range(level):
                 s += '┊\t'
-            return s + str(self.expression) + '\n'
+            return s + str(self.expression) + ' {}\n'.format(self.fname)
         #elif self.name == '○ <<':
         #    for i in range(level):
         #        s += '┊\t'
         #    return s + '○ << ' + self.children[0].name + '\n'
         for i in range(level):
             s += '┊\t'
-        s += '╭ ' + self.name + '\n'
+        s += '╭ {} {}\n'.format(self.name, self.fname)
         level += 1
         for child in self.children:
             if child:
@@ -114,15 +116,33 @@ class Node():
                 free += 1
         return free
 
+    def strip(self):
+        """
+        Removes the rightest leaf.
+        """
+        done = False
+        if self.name in [leaf, link]:
+            return done
+        for i in range(len(self.children) -1, -1, -1):
+            if not done and self.children[i]:
+                if self.children[i].name in [leaf, link]:
+                    self.children[i].parent = None
+                    self.children[i] = None
+                    return True
+                else:
+                    done = strip(self.children[i])
+
+
+
 def wrap(number):
     """Just an alias to `Node('leaf', number)` method, creating a leaf node."""
     return Node(leaf, number)
 
 def fakenode(node):
     """Creates a new node with a link to `node`. Use carefully: high risk of endless recursion."""
-    fl = Node(link, lambda x: x, 1)
-    fl.children[0] = node
-    return fl
+    fn = Node(link, lambda x: x, 1)
+    fn.children[0] = node
+    return fn
 
 def demo():
     print("Abstract syntax tree class demo:")
@@ -165,5 +185,22 @@ def demo():
     print(k.show())
     print("The tree could be easily converted into postfix notation:\n3 5 + 1.25 + 3 5 + * = {}".format(k.render()))
 
+
+def demo_strip():
+    tree = Node('sum3', lambda a, b, c: a + b + c, 3)
+    for i in range(3):
+        tree.grow(wrap(i + 1))
+    print(tree.show())
+    tree.strip()    
+    print(tree.show())
+    tree.strip()    
+    print(tree.show())
+    tree.strip()    
+    print(tree.show())
+    tree.strip()    
+    print(tree.show())
+
+
 if __name__ == '__main__':
     demo()
+    demo_strip()
--- python_tasks/calculator/utils.py
+++ python_tasks/calculator/utils.py
@@ -11,7 +11,7 @@ operators = {
     }
 
 functions = {
-    'sum'   : Node('sum', lambda a, b, c: a + b + c, 3)
+    'id'    : Node('id', lambda x: x, 1, None, 'id(a)')
 }
 
 variables = {
@@ -20,13 +20,24 @@ variables = {
 }
 
 float_re = re.compile(r"^\d+?\.\d+?$")
+varname_re = re.compile(r"^[a-z_]+?$")
+parse_re = re.compile(r'[a-z_]+|[-+*\/\^(),]|\d+\.?\d*')
+
+greeting = (
+    "*****************************************************\n"+
+    "* Arithmetical expressions evaluator                *\n"+
+    "* Author: Dan Iakovlev (moleculadesigner@gmail.com) *\n"+
+    "* 2017                                              *\n"+
+    "*****************************************************\n")
+
+
 
 def parse(expr):
     """
     Parses input expression and returns list of tokens to be processed via shunting yard algorythm.
     """
-    parser = re.compile(r'[a-z_]+|[-+*\/\^(),]|\d+\.?\d*')
-    return parser.findall(expr)
+    return parse_re.findall(expr)
+
 
 def apply(op, q):
     """
@@ -48,13 +59,27 @@ def apply(op, q):
     q.append(op)
 
 
-def makeAST(tokens):
+def make_AST(tokens, params = []):
     """
     Takes a list of infix expression tokens and returns an [Abstrac Syntax Tree](https://en.wikipedia.org/wiki/Abstract_syntax_tree) of this expression using [Shunting-yard algorithm](https://en.wikipedia.org/wiki/Shunting-yard_algorithm).
     """
     stack = []
     queue = []
-    tree = Node('Expression', lambda x: x, 1)
+
+    parameters = {}
+    for p in params:
+        if p in (list(variables.keys()) + list(functions.keys()) + list(parameters.keys())):
+            print("Function parameter ({}) may not be a variable or function name.".format(p))
+            raise SyntaxError
+        else:
+            n = Node(p, lambda x: x, 1)
+            n.grow(wrap('♦'))
+            parameters[p] = [
+                n, # a parameter node with function Id (\x. x) and a dummy leaf. 
+                0 # How many times the parameter was used
+            ]
+            
+
     for token in tokens:
         #print("Token: {}".format(token))
         # Digits go directly to queue
@@ -65,7 +90,17 @@ def makeAST(tokens):
 
         # Variables are handled as numbers
         elif token in variables.keys():
-            queue.append(wrap(variables[token]))
+            v = wrap(variables[token])
+            v.fname = "({})".format(token)
+            queue.append(v)
+
+        # Function parameters
+        elif token in parameters.keys():
+            if parameters[token][1]:
+                queue.append(fakenode(parameters[token][0]))
+            else:
+                queue.append(parameters[token][0])
+            parameters[token][1] += 1
 
         # Functions go to stack
         elif token in functions.keys():
@@ -115,6 +150,10 @@ def makeAST(tokens):
         else:
             apply(stack.pop(), queue)
 
+    if params:
+        for p in parameters.keys():
+            parameters[p][0].strip()
+
     if len(queue) == 1:
         #print(queue[0].show())
         return queue[0]          
@@ -123,17 +162,102 @@ def makeAST(tokens):
         for node in queue:
             print("{}:\n{}".format(node.name, node.show()))
 
-    
-    print(str(queue))
 
-    
+def add_variable(varname: str, tokens: list):
+    if not varname_re.match(varname):
+        print("Invalid variable name: {}".format(varname))
+        raise SyntaxError
+    elif varname in variables.keys():
+        print("Variable {} already exists.".format(varname))
+        raise ValueError
+    else:
+        variables[varname] = make_AST(tokens).render()
+
+
+def add_function(funcname, tokens, params):
+    ast = make_AST(tokens, params)
+    ast.fname = "{}({})".format(funcname, ", ".join(params))
+    functions[funcname] = ast
+
+
+def show_help():
+    print("Help")
+
+def calculator():
+    while True:
+        print(greeting)
+        raw = input("math > ")
+        commands = parse(raw)
+        if not commands:
+            continue
+        elif commands[0] == 'exit':
+            return
+
+        elif commands[0] == 'let':
+            try:
+                add_variable(commands[1], commands[2:])
+            except Exception:
+                continue 
+            print("Remembered {} = {}".format(commands[1], variables[commands[1]]))
+
+        elif commands[0] == 'function':
+            funcname = commands[1]
+            params = []
+            if commands[2] == '(':
+                for c in range(3, len(commands)):
+                    if commands[c] == ')':
+                        close = c + 1
+                        break
+                    elif commands[c] == ',':
+                        continue
+                    else:
+                        params.append(commands[c])
+            else:
+                print("Invalid function definition.")
+            try:
+                add_function(funcname, commands[close:], params)
+            except Exception:
+                continue
+            print("Function {}({}) = {} is saved.".format(funcname, ", ".join(params), "".join(commands[close:])))
+
+        elif commands[0] == 'tree':
+            try:
+               ast = make_AST(commands[1:])
+            except Exception:
+                continue
+            print(ast.show())
+
+        elif commands[0] == 'help':
+            show_help()
+               
+        elif commands[0] == 'reveal':
+            print("Functions:")
+            for f in functions.items():
+                print("{}\t{}".format(f[0], f[1].fname))
+            print("\nVariables:")
+            for v in variables.items():
+                print("{}\t{}".format(v[0], v[1]))
+
+        else:
+            try:
+                ast = make_AST(commands)
+                result = ast.render()
+            except Exception:
+                print("Error")
+                continue
+            print(result)
+
+
 def demo():
-    expr = "sum(1,2,3) * 0.03 + 45 - e^(12*4/56.01)"
+    add_variable('g', ['9.8'])
+    add_function('del', parse('(a + c)/b'), ['a', 'b', 'c'])
+    print(functions['del'].show())
+    expr = "del(2,3, 5) * 0.03 + 45 - e^(12*4/56.01)"
     print(expr)
-    ast = makeAST(parse(expr))
+    ast = make_AST(parse(expr))
     print(ast.show())
     print(ast.render())
 
 if __name__ == '__main__':
-    demo()
+    calculator()
 
