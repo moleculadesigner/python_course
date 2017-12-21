84a1a2464fbc65874d653f6a97b5b58d8f35f255
Danila Iakovlev <moleculadesigner@gmail.com>
2017-12-21 22:39:30 +0700

    Added BWT

---
+++ bioinformatics/bwt.py
@@ -0,0 +1,34 @@
+from operator import itemgetter
+
+
+def bw_transform(s):
+    n = len(s)
+    m = sorted([s[i:n]+s[0:i] for i in range(n)])
+    I = m.index(s)
+    L = ''.join([q[-1] for q in m])
+    return (I, L)
+
+
+def bw_restore(I, L):
+    n = len(L)
+    X = sorted([(i, x) for i, x in enumerate(L)], key=itemgetter(1))
+
+    T = [None for i in range(n)]
+    for i, y in enumerate(X):
+        j, _ = y
+        T[j] = i
+
+    Tx = [I]
+    for i in range(1, n):
+        Tx.append(T[Tx[i-1]])
+
+    S = [L[i] for i in Tx]
+    S.reverse()
+    return ''.join(S)
+
+
+seq = 'ATGCTAAGCTGATGATGGCGGGATG'
+
+print(seq)
+print(str(bw_transform(seq)))
+print(bw_restore(*bw_transform(seq)))
