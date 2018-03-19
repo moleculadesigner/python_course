c30e61e96c0865e5306dbdfc4aff572479a973e3
moleculadesigner <moleculadesigner@gmail.com>
2018-03-19 18:14:00 +0700

    Added orgraphs ---MD

--- python_tasks/graph/nodes.py
+++ python_tasks/graph/nodes.py
@@ -9,8 +9,12 @@ ARROW = Arrow(None, None, None)
 EDGE = Edge(frozenset((None,)), None)
 
 def deprecated(fn):
+    """Raises a `NotImplementedError` exception
+     while attempting to call decorated function."""
     def wrapper(*args, **kwargs):
-        raise NotImplementedError("Function {} is broken.".format(fn))
+        raise NotImplementedError(
+        """Function {}({}, {}) have not been implemented yet.
+        """.format(fn, args, kwargs))
     return wrapper
 
 
@@ -18,8 +22,8 @@ class Node:
     """
     A basic type for graph node representation.
     """
-    def __init__(self, children=[],
-            name=None, content=None):
+    def __init__(self, name=None, children=[],
+            content=None):
         if name:
             self.name = name
         else:
@@ -36,7 +40,7 @@ class Node:
     @property
     def children(self):
         return set([
-            (n.pop() if len(n) == 1 else self) for\
+            ((n.pop(), None) if len(n) == 1 else self) for\
             n in map(lambda x: set(x.nodes) - set((self,)), self.edges)
         ])
 
@@ -47,22 +51,30 @@ class Node:
     def grow(self, node, weight=None):
         if node in self.children:
             return None
-        e = Edge(frozenset((self, node)), None)
+        e = Edge(frozenset((self, node)), w=None)
         self.edges.append(e)
         if node is not self:
             node.edges.append(e)
 
-    def drop(self, node):
-        for edge in self.edges:
-            if set((self, node)) == edge.nodes:
-                self.edges.remove(edge)
-                if node is not self:
-                    node.edges.remove(edge)
+    def drop(self, edge):
+        if edge in self.edges:
+            self.edges.remove(edge)
+            node = edge.nodes - set((self,))
+            if node:
+                node.pop().edges.remove(edge)
+
+    def links(self, node):
+        """returns list of edges linking to the `node`"""
+        links = []
+        for e in self.edges:
+            if node in e.nodes:
+                links.append(e)
+        return links
 
 
 class UNode(Node):
     """
-    A node in unoriented graph
+    A node in unoriented graph.
     """
     def __init__(self, name=None, children=[],
             multi=False, content=None):
@@ -73,10 +85,12 @@ class UNode(Node):
             self.grow(node, w)
 
     def __repr__(self):
-        return "u.{}".format(self.name)
+        return "o.{}".format(self.name)
 
     @property
     def children(self):
+        """Returns list of tuples for
+        all connected nodes and their weights."""
         ch = []
         for e in self.edges:
             if set((self,)) == e.nodes:
@@ -91,16 +105,104 @@ class UNode(Node):
 
     @property
     def deg(self):
+        """
+        Returns degree of the node:
+
+        deg(v) = sum(w(e | e is incident to v))
+        """
         return reduce(lambda x, y: x + y[1],
                       self.children, 0)
 
     def grow(self, node, weight=1):
+        """
+        Addes edge to `node` with `weight`. If `node` is already linked
+        does nothing, unless you are in multigraph mode.
+        """
         if not self.multi and node in map(lambda x: x[0], self.children):
             return None
         e = Edge(set((self, node)), weight)
         self.edges.append(e)
         if node is not self:
             node.edges.append(e)
+
+
+class ONode(Node):
+    """
+    A node in oriented graph.
+    """
+    def __init__(self, name=None, children=[],
+            multi=False, content=None):
+        Node.__init__(self, name=name, content=content)
+        self.multi = multi
+        self.edges = []
+        for node, w in children:
+            self.grow(node, w)
+
+    def __repr__(self):
+        return ">.{}".format(self.name)
+
+    @property
+    def children(self):
+        """Returns list of tuples for
+        all connected nodes and their weights."""
+        ch = []
+        for e in self.edges:
+            if e.start is self:
+                ch.append((
+                    e.end,
+                    e.w)
+                )
+        return ch
+
+    @property
+    def parents(self):
+        p = []
+        for e in self.edges:
+            if e.end is self:
+                p.append((
+                    e.start,
+                    e.w)
+                )
+        return p
+
+    @property
+    def deg(self):
+        """
+        Returns degree of the node:
+
+        deg(v) = sum(w(e | e is incident to v))
+        """
+        return reduce(lambda x, y: x + y[1],
+                      self.children, 0)
+
+    def grow(self, node, weight=1):
+        """
+        Addes edge to `node` with `weight`. If `node` is already linked
+        does nothing, unless you are in multigraph mode.
+        """
+        if not self.multi and node in map(lambda x: x[0], self.children):
+            return None
+        e = Arrow(self, node, weight)
+        self.edges.append(e)
+        if node is not self:
+            node.edges.append(e)
+
+    def drop(self, arrow):
+        if arrow in self.edges:
+            self.edges.remove(arrow)
+            if arrow.end is not self:
+                arrow.end.edges.remove(arrow)
+
+    def links(self, node):
+        """returns list of edges linking to the `node`"""
+        links = []
+        for a in self.edges:
+            if node in (a.start, a.end):
+                links.append(a)
+        return links
+
+
+
 
 
 @deprecated
@@ -126,21 +228,22 @@ def merge(edges, method=(lambda x, y: x + y)):
 
 
 if __name__ == '__main__':
-    a = UNode('a')
-    b = UNode('b', multi=True)
+    a = ONode('a')
+    b = ONode('b')
     print(b.children)
 
-    c = UNode('c', [(a, 5), (b, 1), (a, 4)], multi=True)
-    print(b.children)
+    c = ONode('c', [(a, 5), (b, 1), (a, 4)])
     b.grow(c, 3)
-    print(a.children)
-    print(b.children)
-    print(c.children)
+    print(c.edges)
+    c.drop(c.links(a)[0])
+    print(c.edges)
+    print(a.edges)
+    
     """
-    aa = Node()
-    ab = Node()
-    ac = Node([aa, ab, aa, aa])
-    print(ac)
-    print(ac.edges)
+    """
+    aa = Node('A')
+    ab = Node('B')
+    ac = Node('C', [aa, ab, aa, aa])
     print(ac.children)
-    """
+    print(ac.edges)
+    print(ac.links(aa))
